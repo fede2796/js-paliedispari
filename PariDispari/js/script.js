11ca0204e463c40a5478ec42b508706500f54d84
fede2796 <fede27396@gmail.com>
2022-06-13 16:33:59 +0200

    js modifiche

--- PariDispari/js/script.js
+++ PariDispari/js/script.js
@@ -4,25 +4,42 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
-
+const userEvenorOdd = prompt ('Pari o Dispari');
 const userNumber = parseInt(prompt('Dimmi un numero'));
+const userNumberselect = isEvenOrOdd(userNumber);
 
-const userNumberEvenOrOdd  = isEvenOrOdd(userNumber);
+//Funzione per vedere se il numero dell'utente è pari
+function isEvenOrOdd (number){
+    let evenOrOdd;
 
-const randomcomputer = getRandomComputer(1 , 5);
+    if(number % 2 === 0){
+        evenOrOdd = 'even';
+    }
+    else{
+        evenOrOdd = 'odd';
+    }
+    
+    return evenOrOdd;
+}
 
-if(userNumberEvenOrOdd === 'even') {
+if(userNumberselect === 'even') {
     alert('Numero pari');
 } else {
     alert('Numero dispari');
 }
 
+//Genero un numero Random da 1 a 5 per il computer 
+for(let i = 0; i <= 1 ; i++) {
+    const random = getRndInteger(1, 5);
+    console.log(random);
+}
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function sommanumber(userNumber, getRandomComputer){
-    const sum = userNumber + getRandomComputer ;
+//Sommo i due numeri
+function sommainumeri(userNumber, random){
+    const sum = userNumber + random;
     return sum;
 }
 
